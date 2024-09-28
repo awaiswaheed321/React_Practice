987a51d171c1c27b16d968a8e34c7ace22fc7d61
Awais Waheed <awaiswaheed321@gmail.com>
2024-09-28 11:18:24 -0500

    Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Student from "./Student";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Student name="Sponge Bob" age = {50} isStudent={true}/>
+      <Student name="John Doe" age = {30} isStudent={1}/>
+      <Student name="Ted Lasso" age = "30" isStudent={0}/>
+      <Student name="Coach Beard" age = {45} isStudent={false}/>
+      <Student name="Roy Kent"/>
+    </>
+  );
 }
 
 export default App;
