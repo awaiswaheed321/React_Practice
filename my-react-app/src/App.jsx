22852819031b60d75b9eda60e26079c73dc6191f
Awais Waheed <awaiswaheed321@gmail.com>
2024-09-28 11:00:33 -0500

    inline styling

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./Button/Button";
+import Button from "./Button";
 function App() {
   const [count, setCount] = useState(0);
 
