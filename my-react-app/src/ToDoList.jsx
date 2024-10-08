c5bffd5cd843729c4ef4737f43a57460e3262ede
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-08 14:31:10 -0500

    to do list App

---
+++ my-react-app/src/ToDoList.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    setTasks((t) => t.filter((_, i) => i !== index));
+  };
+
+  const moveTaskUp = (index) => {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  };
+
+  const moveTaskDown = (index) => {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  };
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a Task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              ✖️
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              👆
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              👇
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default ToDoList;
